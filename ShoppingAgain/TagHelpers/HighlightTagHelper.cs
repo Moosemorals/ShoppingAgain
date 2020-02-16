507f2b0504469803b82401e826066a8243a7569a
Osric Wilkinson <gitmoose@fluffypeople.com>
2020-02-16 22:10:57 +0000

    Get tag helpers working again

--- ShoppingAgain/TagHelpers/HighlightTagHelper.cs
+++ ShoppingAgain/TagHelpers/HighlightTagHelper.cs
@@ -8,13 +8,14 @@ using System.Threading.Tasks;
 
 namespace ShoppingAgain.TagHelpers
 {
-    [HtmlTargetElement(Attributes = nameof(Highlight))]
+    [HtmlTargetElement(Attributes = "highlight")]
     public class HighlightTagHelper : TagHelper
     {
         public bool Highlight { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            output.Attributes.RemoveAll("highlight");
             if (Highlight)
             {
                 output.AddClass("highlight", HtmlEncoder.Default);
